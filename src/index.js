50ae8bf669294bc51f70c42375e187efe7168592
Manyara20 <emanyara97@gmail.com>
2023-02-22 00:33:20 +0300

    new

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import SearchMovies from './searchMovie';
+import './App.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div className="container">
+      <nav className="navigation">
+        <img className="image" alt='logo' src="https://zcodes.org/img/z-code.png" />
+        <h1 className="title"> Movie Search</h1>
+        </nav>
+        <SearchMovies/>
+      </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
